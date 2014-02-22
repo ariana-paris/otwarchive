82818eb314d5d24d322dab1ed4e2ba7560b89072
elz <elzj78@gmail.com>
2014-02-21 22:34:23 -0500

    Use Jbuilder to generate work json

--- config/routes.rb
+++ config/routes.rb
@@ -569,6 +569,12 @@ Otwarchive::Application.routes.draw do
       get :about
     end
   end
+  
+  namespace :api do
+    namespace :v1 do
+      resources :works, only: [ :show ]
+    end
+  end
 
   get 'search' => 'works#search'
   post 'support' => 'feedbacks#create', as: 'feedbacks'
