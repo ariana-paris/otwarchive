f532cebb575b9be1c8540d45ffa05d73c196a778
Ariana <ariana@astele.co.uk>
2014-05-24 22:53:10 +0100

    basic_api -  Add initial spec test

---
+++ spec/api/api_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'API response codes' do
+
+  it 'should be 200 when client has an authentication token' do
+    @api = ApiKey.new(name: 'Test', access_token: 'testabc')
+    get '/api/v1/works', nil,
+        { "HTTP_AUTHORIZATION" => ActionController::HttpAuthentication::Token.encode_credentials(@api.access_token),
+          'HTTP_ACCEPT' => "application/json"
+        }
+    assert_equal 200, response.status
+  end
+
+  it 'should return 401' do
+    get '/api/v1/works'
+    assert_equal 401, response.status
+  end
+end
