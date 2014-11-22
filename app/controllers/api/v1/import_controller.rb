ba9dfdd6c872f4df409ee9db130a8b2aa8db0497
Ariana <ariana@astele.co.uk>
2014-11-22 20:15:31 +0000

    basic_api - Handle mulitple chapters; merge in route change for chapter deletion

--- app/controllers/api/v1/import_controller.rb
+++ app/controllers/api/v1/import_controller.rb
@@ -1,41 +1,53 @@
 class Api::V1::ImportController < Api::V1::BaseController
   respond_to :json
 
+  # Imports multiple works with their meta from external URLs
+  # Params:
+  # +params+:: a JSON object containing the login of an archivist and an array of works
   def create
     archivist = User.find_by_login(params[:archivist])
-
+    external_works = params[:works]
+    results = ""
     if archivist && archivist.is_archivist?
-      if params[:urls]
-        storyparser = StoryParser.new
-        results = storyparser.import_from_urls(params[:urls], options(params))
-        render status: :ok, json: {success: true, urls: params[:urls], options: options(params), results: results}
-      else
-        render status: :unprocessable_entity, json: {success: false, errors: 'No URLs were provided for importing' }
+      external_works.each do |work|
+        urls = work[:chapter_urls]
+        if urls.length > 0 && urls.length < ArchiveConfig.IMPORT_MAX_WORKS_BY_ARCHIVIST
+          storyparser = StoryParser.new
+          options = options(archivist, work)
+          @work = storyparser.download_and_parse_chapters_into_story(urls, options)
+          @work.save
+        else
+          results << "No URLs were provided for a work"
+        end
       end
+      # send_external_invites(@works)
+      render status: :ok, json: {success: true, results: results}
     else
       render status: :forbidden, json: {success: false, errors: 'Only an archivist can import works through the API'}
     end
   end
 
 private
-  def options(params)
+  def options(archivist, params)
     {
-            #:pseuds => pseuds_to_apply,
-            :importing_for_others => true,
-            :restricted => params[:restricted],
-            :override_tags => params[:override_tags],
-            :fandom => params[:work][:fandoms],
-            :warning => params[:work][:warnings],
-            :character => params[:work][:characters],
-            :rating => params[:work][:rating],
-            :relationship => params[:work][:relationships],
-            :category => params[:work][:categories],
-            :freeform => params[:work][:additional_tags],
-            :encoding => params[:encoding],
-            :external_author_name => params[:external_author_name],
-            :external_author_email => params[:external_author_email],
-            :external_coauthor_name => params[:external_coauthor_name],
-            :external_coauthor_email => params[:external_coauthor_email]
-        }
+      :archivist => archivist,
+      :import_multiple => 'chapters',
+      :importing_for_others => true,
+      :do_not_set_current_author => true,
+      :restricted => params[:restricted],
+      :override_tags => params[:override_tags],
+      :fandom => params[:fandoms],
+      :warning => params[:warnings],
+      :character => params[:characters],
+      :rating => params[:rating],
+      :relationship => params[:relationships],
+      :category => params[:categories],
+      :freeform => params[:additional_tags],
+      :encoding => params[:encoding],
+      :external_author_name => params[:external_author_name],
+      :external_author_email => params[:external_author_email],
+      :external_coauthor_name => params[:external_coauthor_name],
+      :external_coauthor_email => params[:external_coauthor_email]
+    }
   end
 end
