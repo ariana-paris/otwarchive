f81cf84c4f8d085ce2a3bbba98b9949b8fbdd57e
Ariana <ariana@astele.co.uk>
2016-11-18 08:55:52 +0000

    AO3-4716 - POST the bulk email form data rather than GET

--- config/routes.rb
+++ config/routes.rb
@@ -153,6 +153,7 @@ Otwarchive::Application.routes.draw do
       end
       collection do
         get :notify
+        get :bulk_search
         post :bulk_search
         post :send_notification
         post :update_user
